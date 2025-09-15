aa8c6c708cdb2f007e9a7cca270734b896bcb5f9
David Ramos <davidfernandoramos010@gmail.com>
2025-09-15 22:53:10 +0200

    Ajustes en definicion de docker

--- src/config/scribe.php
+++ src/config/scribe.php
@@ -16,7 +16,7 @@ return [
 
     // The base URL displayed in the docs.
     // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-    'base_url' => config("app.url"),
+    'base_url' => env('APP_URL', 'http://localhost:8000'),
 
     // Routes to include in the docs
     'routes' => [
